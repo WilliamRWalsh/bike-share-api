2942f259fff358dde33a1c24ae9f606f6b5c923d
WilliamRWalsh <williamrobertsonwalsh@gmail.com>
2020-01-15 23:03:15 -0500

    typescript working. Added basic user dir struct

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,23 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
+import express = require('express');
+import morgan = require('morgan');
 
-app.use(morgan('tiny'))
+const userTest = require('./controllers/user-controller')
+
+const app = express();
+const router = express.Router();
+
+/* Pipeline */
+app.use(morgan('tiny'));
+app.use('/api', router);
+
+/* Sanity Check */
+router.get('/ok', (request, response) => {
+    userTest()
+    response.status(200).send();
+})
+
+router.get('/okk', userTest)
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,5 @@
+module.exports = userTest;
+
+function userTest() {
+    console.log('okay')
+}
