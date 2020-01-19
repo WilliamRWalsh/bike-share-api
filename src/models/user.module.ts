952706449b4663e9a58cee55d2ae8bfe7d82bee1
WilliamRWalsh <williamrobertsonwalsh@gmail.com>
2020-01-19 17:29:08 -0500

    Added mognoose and user model

---
+++ src/models/user.module.ts
@@ -0,0 +1,10 @@
+import mongoose = require('mongoose');
+
+const User = mongoose.model('User', new mongoose.Schema({
+    phone: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 10,
+    }
+}));
